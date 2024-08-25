099055e0f71d6d109abb571aa856d183ccfc5aea
Felix Allard <xilef992@gmail.com>
2024-08-25 11:58:49 -0400

    The application is working officialy

---
+++ PatientManagementSystem/Components/Model/Appointment.cs
@@ -0,0 +1,9 @@
+namespace PatientManagementSystem.Components.Model;
+
+public class Appointment
+{
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public DateTime Date { get; set; }
+    public string Description { get; set; }
+}
